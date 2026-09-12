60453fb0210cdcb06d499a0e8bd345a90276dfa7
Pyae Sone Htun <123477615+PyaeSoneHtun-98@users.noreply.github.com>
2026-09-12 23:21:44 +0630

    Expose secure player control bridge

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,11 +1,19 @@
-import { contextBridge, ipcRenderer, type IpcRendererEvent } from 'electron'
+import { contextBridge, ipcRenderer, type IpcRendererEvent, webUtils } from 'electron'
 import type { DesktopBridge, PlaybackSnapshot } from '../shared/media'
 
 const desktopBridge: DesktopBridge = {
   platform: process.platform,
   media: {
     openVideo: () => ipcRenderer.invoke('media:open-video'),
+    openVideoPath: (filePath) => ipcRenderer.invoke('media:open-video-path', filePath),
+    getPathForFile: (file) =>
+      webUtils.getPathForFile(file as Parameters<typeof webUtils.getPathForFile>[0]),
     getState: () => ipcRenderer.invoke('media:get-state'),
+    setPaused: (paused) => ipcRenderer.invoke('media:set-paused', paused),
+    seek: (seconds) => ipcRenderer.invoke('media:seek', seconds),
+    setVolume: (volume) => ipcRenderer.invoke('media:set-volume', volume),
+    setSpeed: (speed) => ipcRenderer.invoke('media:set-speed', speed),
+    toggleFullscreen: () => ipcRenderer.invoke('media:toggle-fullscreen'),
     onState: (listener) => {
       const subscription = (_event: IpcRendererEvent, state: PlaybackSnapshot): void => listener(state)
       ipcRenderer.on('media:state', subscription)
